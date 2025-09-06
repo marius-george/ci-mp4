d5ccfb7c360dfb3831a785d926336b3a3abfdc13
Marius George <marius.olariu@hotmail.com>
2025-09-06 19:01:51 +0100

    Modified settings

--- eshop_mp4/settings.py
+++ eshop_mp4/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'b1oej^%_=*t22903_gz_ss1n*4o8%yus7@!ias77ij-td93m)*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
