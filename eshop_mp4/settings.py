7266cc5c3df7e4ce53a2de81f4b807a8c402ef06
marius-george <marius.olariu@hotmail.com>
2025-09-09 07:12:36 +0100

    modified requirements.txt for deployment

--- eshop_mp4/settings.py
+++ eshop_mp4/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django-crispy-forms',
     'crispy_bootstrap4',
 
     # third-party
