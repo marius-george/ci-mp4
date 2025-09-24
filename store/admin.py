435bed0553785a49294bc9126c57dd2a4c4c12c2
marius-george <marius.olariu@hotmail.com>
2025-09-24 20:50:20 +0100

    Changes in admin

--- store/admin.py
+++ store/admin.py
@@ -3,5 +3,11 @@ from .models import Category, Product
 
 # Register your models here.
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'stock', 'available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+    list_per_page = 10
+
 admin.site.register(Category)
 admin.site.register(Product)
