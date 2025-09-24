712021a3f79da8023a76990b5e184fab5567116d
marius-george <marius.olariu@hotmail.com>
2025-09-24 20:54:01 +0100

    admin panel

--- store/admin.py
+++ store/admin.py
@@ -3,11 +3,13 @@ from .models import Category, Product
 
 # Register your models here.
 
+admin.site.register(Category)
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'price', 'stock', 'available', 'created', 'updated']
     list_editable = ['price', 'stock', 'available']
     prepopulated_fields = {'slug': ('name',)}
     list_per_page = 10
 
-admin.site.register(Category)
+    
 admin.site.register(Product)
